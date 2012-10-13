6aad82b089a728bd0fa1f3247cc8df63f8effbf4
Krzysztof Krol <krzysztofkrol@yahoo.pl>
2012-10-13 18:20:00 +0200

    Heroku problem with root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Shop::Application.routes.draw do
 root :to => "shop#index"
-resources :categories do
-  resource :list
-end
+resources :categories
 resources :products
-
+resources :shop
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
